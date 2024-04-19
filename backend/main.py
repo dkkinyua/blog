f1297ba1d8493bf8baf5456f95af1c4524e10388
unknown <denzelkinyua11@gmail.com>
2024-04-19 22:54:49 +0300

    Commit #3: Creating db, making migrations and upgrading the db

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_restx import Api, Resource
+from flask_migrate import Migrate
 from config import DevelopmentConfig
+from models import User, Post
+from exts import db
+
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+db.init_app(app)
+migrate = Migrate(app, db)
 api = Api(app, doc="/docs")
 
 @api.route("/hello")
@@ -13,6 +20,14 @@ class HelloResource(Resource):
         }
 
         return message
+    
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "User": User,
+        "Post": Post
+    }
 
 if __name__ == "__main__":
     app.run()
