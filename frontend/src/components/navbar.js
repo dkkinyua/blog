ee49c0998cb7aebc5465af3b08e990e3e3c4edf3
unknown <denzelkinyua11@gmail.com>
2024-04-25 20:27:38 +0300

    Commit #16b: Added a collapse navbar to the button on small screens

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,33 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Link } from 'react-router-dom'; 
 
-const NavBar = () => {
+function Navbar() {
+    
+    const [isNavbarCollapsed, setIsNavbarCollapsed] = useState(true);
+    
+    const toggleNavbar = () => {
+        setIsNavbarCollapsed(!isNavbarCollapsed);
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg bg-secondary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <Link className="navbar-brand" href="#">Blooog</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <Link className="navbar-brand" to="#">Blooog</Link>
+                <button className="navbar-toggler" type="button" onClick={toggleNavbar}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
-                    <ul className="navbar-nav">
+                <div className={`collapse navbar-collapse ${isNavbarCollapsed ? '' : 'show'}`} id="navbarNav">
+                    <ul className="navbar-nav me-auto">
                         <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" href="#">Home</Link>
+                            <Link className="nav-link active" aria-current="page" to="#">Home</Link>
                         </li>
                     </ul>
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link className="nav-link" href="#">Login</Link>
+                            <Link className="nav-link" to="#">Login</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" href="#">Create Post</Link>
+                            <Link className="nav-link" to="#">Signup</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="#">Create Post</Link>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-
-    )
+    );
 }
 
-export default NavBar
+export default Navbar;
